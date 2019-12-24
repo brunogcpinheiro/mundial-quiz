194f6602343bc277f0929e5ff745aa4e6d9fe6ac
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2019-12-24 14:43:41 +0000

    refactor functions

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const data = {
     time2: "chelsea"
   },
   2005: {
-    time1: "sao paulo",
-    time2: "liverpool"
+    time1: "liverpool",
+    time2: "sao paulo"
   },
   1993: {
     time1: "sao paulo",
@@ -15,8 +15,6 @@ const data = {
 
 function render(time, position) {
   const el = document.querySelectorAll(position);
-
-  console.log(el);
 
   el.forEach(e => {
     e.innerHTML = time;
@@ -43,22 +41,23 @@ function reset(input) {
 }
 
 function compare(time) {
-  // CHECAR SE O TIME ESTA DENTRO DO OBJETO COM A KEY DO ANO RESPECTIVO
-  console.log(Object.entries(data)[2])
+  compareAndRender(time, "1993", 0);
+  compareAndRender(time, "2005", 1);
+  compareAndRender(time, "2012", 2);
+}
 
-  const casa = Object.values(data)[2].time1;
-  const fora = Object.values(data)[2].time2;
+function compareAndRender(time, year, index) {
+  if(Object.entries(data)[index][0] === year) {
+    const home = Object.values(data)[index].time1;
+    const away = Object.values(data)[index].time2;
 
-  console.log(casa, fora);
-
-  if(Object.entries(data)[0][0] === "2012") {    
-    if(casa === time) {
-      render(time, "#home_club");
-    } else if(fora === time) {
-      render(time, "#away_club");
-    } else {
-      console.log("errou");
+    if(home === time) {
+      render(time, `.home_${year}`);
+    } else if(away === time) {
+      render(time, `.away_${year}`);
     }
+  } else {
+    alert("Something goes wrong!");
   }
 }
 
